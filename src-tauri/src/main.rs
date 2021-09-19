a2f7642355dc7c571227c490f2f0824dc8177a67
grant0417 <grant@ggurvis.com>
2021-09-19 04:36:10 -0400

    Termial is now functional

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,32 @@
   windows_subsystem = "windows"
 )]
 
-use std::{borrow::BorrowMut, cell::RefCell, error::Error, ffi::CString, io::Write, os::unix::prelude::FromRawFd, sync::{Arc, Mutex}};
+use std::{borrow::BorrowMut, env::vars, error::Error, ffi::CString, fs::File, io::{BufReader, Read, Write}, os::unix::prelude::FromRawFd, sync::{Arc, Mutex}};
 
-use nix::{
-  ioctl_none_bad,
-  libc::{close, TIOCSCTTY},
-  pty::openpty,
-  unistd::{dup2, execvpe, fork, setsid, ForkResult},
-};
+use nix::{ioctl_none_bad, libc::{close, TIOCSCTTY}, pty::openpty, unistd::{ForkResult,  dup2, execvpe, fork, setsid}};
 
 ioctl_none_bad!(tiocsctty, TIOCSCTTY);
 
-use tauri::Manager;
+use tauri::{Manager, Window};
+
+fn send_output(window: Arc<Window>, file_fd: i32) {
+  std::thread::spawn(move || {
+    let file = unsafe { File::from_raw_fd(file_fd) };
+    let mut buff_reader = BufReader::new(file);
+    loop {
+      let mut buffer = [0; 1];
+      match buff_reader.read(&mut buffer) {
+        Ok(_) => {
+          window.emit("write", buffer[0] as char).unwrap();
+        }
+        Err(_) => {
+          window.emit("close", "").unwrap();
+          break;
+        }
+      }
+    }
+  });
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
   let pty_res = openpty(None, None)?;
@@ -25,18 +39,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     ForkResult::Parent { child: _ } => {
       unsafe { close(pty_res.slave) };
 
-      let master = Arc::new(Mutex::new(unsafe { std::fs::File::from_raw_fd(pty_res.master) }));
-
       tauri::Builder::default()
       .setup(move |app| {
-        let master_clone = Arc::clone(&master);
+        let main_window = Arc::new(app.get_window("main").unwrap());
+        let master = Arc::new(Mutex::new(unsafe { File::from_raw_fd(pty_res.master) }));
+
         app.listen_global("data", move |event| {
-          println!("got event-name with payload {:?}", event.payload());
           if let Some(data) = event.payload() {
-            let master_clone = Arc::clone(&master_clone);
-            let mut master_unlock = master_clone.lock().unwrap();
-            master_unlock.borrow_mut().write(data.as_bytes()).unwrap();
+            let master_clone = Arc::clone(&master);
+            master_clone.lock().unwrap().borrow_mut().write(data.as_bytes()).unwrap();
           }
+        });
+
+        app.listen_global("ready", move |_| {
+          send_output(Arc::clone(&main_window), pty_res.master);
         });
   
         Ok(())
@@ -57,7 +73,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
       unsafe { close(pty_res.slave) };
 
-      execvpe::<CString, CString>(&CString::new("/bin/sh")?, &[], &[])?;
+      // let usr = User::from_uid(getuid()).unwrap_or(User::from_name("root").unwrap()).unwrap();
+
+      let sh = CString::new("/bin/sh")?;
+
+      let mut env_vars = vars()
+        .filter(|(key, _)| key != "TERM")
+        .map(|(k, v)| CString::new(format!("{}={}", k, v)).unwrap()).collect::<Vec<_>>();
+
+      env_vars.push(CString::new("TERM=xterm-256color")?);
+      
+      execvpe::<CString, CString>(&sh, &[], &env_vars)?;
     }
   }
 
--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,25 @@ import './App.css';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { getCurrent } from '@tauri-apps/api/window'
+import { listen } from '@tauri-apps/api/event'
 
 const current = getCurrent();
 
 function App() {
   const terminalDiv = useRef<HTMLDivElement>(null);
   const term = new Terminal();
+  term.setOption('fontFamily', 'Hack');
   const fitAddon = new FitAddon();
   term.loadAddon(fitAddon);
 
   useEffect(() => {
     if (terminalDiv.current) {
       term.open(terminalDiv.current);
-      term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $\r\n');
       fitAddon.fit();
       term.onData((data) => {
         current.emit('data', data);
-        term.write(data);
       });
+      current.emit('ready', '');
     }
   }, []);
 
@@ -29,6 +30,16 @@ function App() {
       fitAddon.fit();
     }
   });
+
+  listen('write', event => {
+    console.log('write', event);
+    term.write(event.payload as string);
+  });
+
+  listen('close', event => {
+    term.dispose();
+    current.close();
+  })
 
   return <div className="terminal" ref={terminalDiv}></div>;
 }
